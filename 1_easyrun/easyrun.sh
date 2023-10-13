c8ee0bcb7e901321152ac7f7b37bd9d6a98e7ddd
Betswish <131008659+Betswish@users.noreply.github.com>
2023-10-13 13:30:55 +0200

    Update easyrun.sh

--- 1_easyrun/easyrun.sh
+++ 1_easyrun/easyrun.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# The models you want to test on
+# The model you want to test on
 model=bigscience/bloom-3b # bigscience/bloom-3b google/mt5-large xlm-roberta-large
-
 # Language pair
 lang1=en # en fr nl es ru ja zh ko vi el hu he tr ca ar uk fa
 lang2=fr # en fr nl es ru ja zh ko vi el hu he tr ca ar uk fa
-
-# BMLAMA17
-mini=yes # no, using BMLAMA53
-
-
-# Run Probing
-
-if [ ! -f "$file" ]; then
-  touch "$file"
-fi
-
-python main.py -lang $lang1 -mname $model -mini $mini
-python main.py -lang $lang2 -mname $model -mini $mini
-
-# Calculating CLC with RankC
+# Dataset
+mini=yes # 'yes' for using BMLAMA17; 'no' for using BMLAMA53
+# Weight Metric
 weight=softmax # softmax norm1 norm2
 
+# Run Probing (No need for changing)
+for LANG in $lang1 $lang2
+do
+  file=
+  if [ ! -f "$file" ]; then
+    touch "$file"
+  fi
+  python main.py -lang $LANG -mname $model -mini $mini
+done
+
+# Calculating CLC with RankC (No need for changing)
 echo $model $lang1 $lang2
 python RankC.py -pair $lang1,$lang2 -mname $mname -weight $weight -mini $mini
